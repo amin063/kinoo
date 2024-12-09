d2fd2a82220f1482d0e249edc9d02c7616aa1b51
amin063 <aminaliyev630@gmail.com>
2024-12-10 01:05:55 +0400

    Added No Data Note

--- src/pages/favlists/FavLists.jsx
+++ src/pages/favlists/FavLists.jsx
@@ -4,6 +4,7 @@ import { IoMdRemoveCircle } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
 function FavLists() {
+  
   const [favList, setFavList] = useState([]);
   const userId = localStorage.getItem("userId")
 
@@ -21,25 +22,32 @@ function FavLists() {
 
 
   return (
-    <div className='w-[90%]  m-auto flex flex-col justify-center items-center gap-10'>
-      <h1 className='font-bold text-2xl'>My List</h1>
-      {
-        favList.map((fav) => {
-          return (
-            <div className='flex gap-5 items-center w-[100%]'>
-              <Link
-                to={`/favlist/${fav.id}`}
-                className='w-[80%] rounded-md  p-3 border border-red-600' key={fav.id}>
-                <p className='font-bold'>{fav.name}</p>
-              </Link>
-                <IoMdRemoveCircle 
-                onClick={() => deleteList(fav.id)} className='text-[25px] text-red-600' />
-            </div>
+    favList?.length
+      ?
+      (
+        < div className='w-[90%]  m-auto flex flex-col justify-center items-center gap-10' >
+          <h1 className='font-bold text-2xl'>My List</h1>
+          {
+            favList.map((fav) => {
+              return (
+                <div className='flex gap-5 items-center w-[100%]'>
+                  <Link
+                    to={`/favlist/${fav.id}`}
+                    className='w-[80%] rounded-md  p-3 border border-red-600' key={fav.id}>
+                    <p className='font-bold'>{fav.name}</p>
+                  </Link>
+                  <IoMdRemoveCircle
+                    onClick={() => deleteList(fav.id)} className='text-[25px] text-red-600' />
+                </div>
 
-          )
-        })
-      }
-    </div>
+              )
+            })
+          }
+        </div >
+      )
+      :
+      <div className='flex justify-center items-center h-[70vh]'>No favorite lists found</div>
+
   )
 }
 
